476fcad6336de3eae2a3d69b63a745cf0e6c6f8f
brianheath <brian.c.heath@gmail.com>
2019-05-30 23:23:59 -0600

    Uploading change for testing purposes.

--- YogicTransformation/public/js/scripts.js
+++ YogicTransformation/public/js/scripts.js
@@ -234,14 +234,14 @@ $(document).ready(function() {
      *  This will hide the 'Refresh Header/Footer' buttons and toggle the 
      *  caret icons from down to right.
      **/
-    $('.toggle-code').click(function() {
+//    $('.toggle-code').click(function() {
 //        var target = $(this).data('target');
 //        
 //        $(target).slideToggle();
 //        $(target).next('button').toggle();
 //        $(this).prev('i.fas').toggleClass('fa-caret-right');
 //        $(this).prev('i.fas').toggleClass('fa-caret-down');
-    });
+//    });
     
     /* An attempt at allowing the tab key to insert a tab */
     $('textarea.code-box').on('keydown', function(event) {
